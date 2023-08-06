720cd3365e04115a281fbc9eea012e49cc74aff3
BeanyTheCoder <alexanderafoko@gmail.com>
2023-08-06 00:08:29 +0000

    some updates to support modules

--- script.mjs
+++ script.mjs
@@ -4,7 +4,7 @@
 // or alternatively, use node.js:
 // press ctrl+shift+` to start a new terminal, then navigate into this folder(using cd) and type "node script.js"
 
-class Stack {
+export default class Stack {
   constructor(initialValue) {
     // if initialValue is not an array, or no value is passed, just set this.dataStore to an empty array(instead of undefined or others)
     if (Array.isArray(initialValue) !== true) {
@@ -19,22 +19,22 @@ class Stack {
     // if value passed in an array, destructure and add each element individually
     if (Array.isArray(value)) {
       value.forEach((element) => {
-        this.dataStore[this.stackSize++] = element;
+        this.dataStore[this.size++] = element;
       });
     } else {
       // else, add the value
-      this.dataStore[this.stackSize++] = value;
+      this.dataStore[this.size++] = value;
     }
   }
 
   pop() {
-    // removes element at the end of the stack, decrements this.stackSize, then return the value removed
-    return this.dataStore.splice(this.stackSize-- - 1, 1);
+    // removes element at the end of the stack, decrements this.size, then return the value removed
+    return this.dataStore.splice(this.size-- - 1, 1);
   }
 
   peek() {
     // returns the value the end(technically called top) of the stack
-    return this.dataStore[this.stackSize - 1];
+    return this.dataStore[this.size - 1];
   }
 
   clear() {
